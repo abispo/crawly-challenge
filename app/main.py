111c9c9c112a183e498b8da3f40d9cadf0ccaf91
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2019-11-01 18:07:57 -0300

    * Adicionado método para gerar token para o POST * Adicionado método que extrai a resposta da página * Adicionado testes para get_answer e transform_token

--- app/main.py
+++ app/main.py
@@ -7,6 +7,27 @@ import aiohttp
 app = FastAPI()
 URL = config('CRAWLY_APP_URL')
 
+headers = {
+    'Host': 'applicant-test.us-east-1.elasticbeanstalk.com',
+    'User-Agent': 'Crawly Challenge Browser',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'http://applicant-test.us-east-1.elasticbeanstalk.com',
+    'Referer': 'http://applicant-test.us-east-1.elasticbeanstalk.com/',
+}
+
+
+async def transform_token(token: str) -> str:
+    replacements = {
+        'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u',
+        'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o',
+        'm': 'n', 'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i',
+        's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c',
+        'y': 'b', 'z': 'a', '0': '9', '1': '8', '2': '7', '3': '6',
+        '4': '5', '5': '4', '6': '3', '7': '2', '8': '1', '9': '0'
+    }
+
+    return ''.join([replacements.get(char) for char in token])
+
 
 async def get_token(session: aiohttp.ClientSession) -> str:
     try:
@@ -18,11 +39,27 @@ async def get_token(session: aiohttp.ClientSession) -> str:
     except Exception as e:
         raise aiohttp.ClientError(str(e))
 
+async def get_answer(session: aiohttp.ClientSession, token: str) -> str:
+    data = {'token': token}
+
+    try:
+        async with session.post(URL, data=data, headers=headers) as response:
+            soup = BeautifulSoup(await response.text(), 'html.parser')
+
+            return soup.find('span', {'id': 'answer'}).text
+    except Exception as e:
+        raise aiohttp.ClientError(str(e))
+
 
 @app.get("/")
 async def index():
     async with aiohttp.ClientSession() as session:
 
         token = await get_token(session)
+        answer = await get_answer(session, await transform_token(token))
 
-        return {"token": token}
+        response = {
+            'answer': answer
+        }
+
+        return response
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,34 +2,54 @@ import os
 import sys
 
 import aiohttp
-
 import pytest
 
+from app.main import get_token, get_answer, transform_token
 from asynctest import CoroutineMock, patch
-from app.main import get_token
+
+
+@pytest.mark.asyncio
+async def test_transform_token():
+    token = "uu920z349z545v3607x6u4yx16uww383"
+    expected = "ff079a650a454e6392c3f5bc83fdd616"
+
+    assert expected == await transform_token(token)
 
 
 @pytest.mark.asyncio
 @patch('aiohttp.ClientSession.get')
 async def test_get_token(mock_get):   
     mock_get.return_value.__aenter__.return_value.text = CoroutineMock(side_effect=[
-    """
-    <html>
-    <head>
-        <script src="adpagespeed.js"></script>
-    </head>
-    <body>
-    <form action="/" method="post" id="form">
-        <input type="hidden" name="token" id="token" value="969w1v3u8yz313v4wuv0907712v6395u" />
-        <input type="button" value="Descobrir resposta" onClick="findAnswer()">
-    </form>
-    </body>
-    </html>
-    """
+        """
+        <html>
+        <head>
+            <script src="adpagespeed.js"></script>
+        </head>
+        <body>
+        <form action="/" method="post" id="form">
+            <input type="hidden" name="token" id="token" value="969w1v3u8yz313v4wuv0907712v6395u" />
+            <input type="button" value="Descobrir resposta" onClick="findAnswer()">
+        </form>
+        </body>
+        </html>
+        """
     ])
-       
 
     async with aiohttp.ClientSession() as session:
         retorno = await get_token(session)
 
         assert retorno == '969w1v3u8yz313v4wuv0907712v6395u'
+
+
+@pytest.mark.asyncio
+@patch('aiohttp.ClientSession.post')
+async def test_get_answer(mock_post):   
+    mock_post.return_value.__aenter__.return_value.text = CoroutineMock(side_effect=[
+        """
+        RESPOSTA: <span id="answer">11</span><br /><a href="http://applicant-test.us-east-1.elasticbeanstalk.com/">Voltar</a>
+        """
+    ])
+    async with aiohttp.ClientSession() as session:
+        retorno = await get_answer(session, '969w1v3u8yz313v4wuv0907712v6395u')
+
+        assert retorno == '11'
